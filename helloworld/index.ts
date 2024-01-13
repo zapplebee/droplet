73933eee9417f10c9139d4875cc6f292696ff9ab
Zac Skalko <zskalko@gmail.com>
2024-01-13 06:48:02 +0000

    add devmode

--- helloworld/index.ts
+++ helloworld/index.ts
@@ -1,13 +1,5 @@
-Bun.serve({
+const PRODUCTION_CONFIG = {
   port: 443,
-  hostname: "0.0.0.0",
-  fetch(req) {
-    return new Response(Bun.file("/mnt/volume_sfo3_01/apps/notes/setting-up-the-droplet.md"), {
-      headers: {
-        'Content-type': 'text/markdown; charset=utf-8'
-      }
-    });
-  },
   tls: {
     cert: Bun.file(
       "/etc/letsencrypt/live/zapplebee.prettybirdserver.com/cert.pem",
@@ -16,12 +8,38 @@ Bun.serve({
       "/etc/letsencrypt/live/zapplebee.prettybirdserver.com/privkey.pem",
     ),
   },
-});
+} as const;
+
+const DEV_CONFIG = {
+  port: 3100
+} as const;
+
+const IS_PRODUCTON = process.env.NODE_ENV === 'production';
+
+const LIVE_CONFIG = IS_PRODUCTON ? PRODUCTION_CONFIG : DEV_CONFIG;
 
 Bun.serve({
-  port: 80,
+
   hostname: "0.0.0.0",
   fetch(req) {
-    return Response.redirect(`${req.url.replace(/^http:/gi, "https:")}`, 302);
+    return new Response(Bun.file("/mnt/volume_sfo3_01/apps/notes/setting-up-the-droplet.md"), {
+      headers: {
+        'Content-type': 'text/markdown; charset=utf-8'
+      }
+    });
   },
+  ...LIVE_CONFIG
 });
+
+
+if(IS_PRODUCTON) {
+  // no need to serve the redirects if we're not in prod
+  Bun.serve({
+    port: 80,
+    hostname: "0.0.0.0",
+    fetch(req) {
+      return Response.redirect(`${req.url.replace(/^http:/gi, "https:")}`, 302);
+    },
+  });
+  
+}
