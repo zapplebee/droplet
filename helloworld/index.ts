1f478f7a8182f79742b7df9a55494af5d9c9b52e
Zac Skalko <zskalko@gmail.com>
2024-01-13 06:49:25 +0000

    prettier

--- helloworld/index.ts
+++ helloworld/index.ts
@@ -11,28 +11,29 @@ const PRODUCTION_CONFIG = {
 } as const;
 
 const DEV_CONFIG = {
-  port: 3100
+  port: 3100,
 } as const;
 
-const IS_PRODUCTON = process.env.NODE_ENV === 'production';
+const IS_PRODUCTON = process.env.NODE_ENV === "production";
 
 const LIVE_CONFIG = IS_PRODUCTON ? PRODUCTION_CONFIG : DEV_CONFIG;
 
 Bun.serve({
-
   hostname: "0.0.0.0",
   fetch(req) {
-    return new Response(Bun.file("/mnt/volume_sfo3_01/apps/notes/setting-up-the-droplet.md"), {
-      headers: {
-        'Content-type': 'text/markdown; charset=utf-8'
-      }
-    });
+    return new Response(
+      Bun.file("/mnt/volume_sfo3_01/apps/notes/setting-up-the-droplet.md"),
+      {
+        headers: {
+          "Content-type": "text/markdown; charset=utf-8",
+        },
+      },
+    );
   },
-  ...LIVE_CONFIG
+  ...LIVE_CONFIG,
 });
 
-
-if(IS_PRODUCTON) {
+if (IS_PRODUCTON) {
   // no need to serve the redirects if we're not in prod
   Bun.serve({
     port: 80,
@@ -41,5 +42,4 @@ if(IS_PRODUCTON) {
       return Response.redirect(`${req.url.replace(/^http:/gi, "https:")}`, 302);
     },
   });
-  
 }
