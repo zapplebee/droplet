01afa314e3cf3916cfed55255a9d71003dbc6578
Zac Skalko <zskalko@gmail.com>
2024-01-14 23:19:59 -0600

    add more headers

--- helloworld/files.ts
+++ helloworld/files.ts
@@ -25,16 +25,34 @@ export async function getAsHtml(): Promise<string> {
 
   let inCodeBlock = false;
 
+  const description = "Zac's Log";
+  const title = "Zac's Log";
+  const canonicalURL = "https://zapplebee.prettybirdserver.com";
   const headTags = `
 <title>zapplebee.prettybirdserver.com</title>
+<meta charset="utf-8" />
 <meta name="viewport" content="width=400px, initial-scale=1" />
-<meta name="description" content="Zac Skalko's Log, desc">
-<meta property="og:title" content="Zac Skalko's Log">
-<meta property="og:type" content="article" />
-<meta property="og:url" content="https://zapplebee.prettybirdserver.com">
-<meta property="og:description" content="A full log of Zac's fun little computer project, og:desc">
-<meta property="og:site_name" content="zapplebee.prettybirdserver.com">
-<meta name="twitter:site" content="@zapplebee">
+<!-- Primary Meta Tags -->
+<title>${title}</title>
+<meta name="title" content="${title}" />
+<meta name="description" content="${description}" />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="${canonicalURL}" />
+<meta property="og:title" content="${title}" />
+<meta property="og:description" content="${description}" />
+<!-- <meta property="og:image" content="NONE_SET" /> -->
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="${canonicalURL}" />
+<meta property="twitter:title" content="${title}" />
+<meta property="twitter:description" content="${description}" />
+<!--  <meta property="twitter:image" content="NONE_SET" /> -->
+
+<!-- Mastodon -->
+<link href="https://mastodon.cloud/@zapplebee" rel="me" />
 <style>@import "/public/main.css";</style>
 `;
 
