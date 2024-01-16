d870fc5f825beaaa04970487faca72cc9558ef53
Zac Skalko <zskalko@gmail.com>
2024-01-15 20:12:54 -0600

    remove test

--- helloworld/files.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "bun:test";
-
-import { getFilePaths, NOTES_DIRECTORY } from "./files";
-
-test("it should read the files", async () => {
-  const result = await getFilePaths();
-  expect(Array.isArray(result)).toBe(true);
-  expect(result.some((e) => !e.startsWith(NOTES_DIRECTORY))).toBe(false);
-});
