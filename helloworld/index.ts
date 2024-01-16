bd9d6a3ade863b3a6c7d0cf6c2c50e9ef8114cc2
Zac Skalko <zskalko@gmail.com>
2024-01-15 21:31:32 -0600

    remove branches

--- helloworld/index.ts
+++ helloworld/index.ts
@@ -4,27 +4,27 @@ import { mainFetchHandler } from "./main";
 
 console.log({ IS_PRODUCTON });
 
-if (IS_PRODUCTON) {
-  Bun.serve({
-    hostname: "0.0.0.0",
-    port: 443,
-    tls: {
-      cert: Bun.file(join(CERTS_DIR, FQDN, `fullchain.pem`)),
-      key: Bun.file(join(CERTS_DIR, FQDN, `privkey.pem`)),
-    },
-    fetch: mainFetchHandler,
-  });
-  // no need to serve the redirects if we're not in prod
-  Bun.serve({
-    port: 80,
-    hostname: "0.0.0.0",
-    fetch(req) {
-      return Response.redirect(`${req.url.replace(/^http:/gi, "https:")}`, 302);
-    },
-  });
-} else {
-  console.log("Not in production mode");
+if (!IS_PRODUCTON) {
+  console.log("Do not run prod server outside production");
   process.exit(1);
 }
+
+Bun.serve({
+  hostname: "0.0.0.0",
+  port: 443,
+  tls: {
+    cert: Bun.file(join(CERTS_DIR, FQDN, `fullchain.pem`)),
+    key: Bun.file(join(CERTS_DIR, FQDN, `privkey.pem`)),
+  },
+  fetch: mainFetchHandler,
+});
+
+Bun.serve({
+  port: 80,
+  hostname: "0.0.0.0",
+  fetch(req) {
+    return Response.redirect(`${req.url.replace(/^http:/gi, "https:")}`, 302);
+  },
+});
 
 console.log("started: " + performance.now());
